0bce9ed7299b42b00f1d79702055161c7b62efb8
Caio Teixeira <caioteixeira.dev@gmail.com>
2023-05-17 10:39:58 -0300

    Add Item Chest

---
+++ Source/ActionRogueLike/Public/CTItemChest.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CTGameplayInterface.h"
+#include "GameFramework/Actor.h"
+#include "CTItemChest.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ACTItemChest : public AActor, public ICTGameplayInterface
+{
+	GENERATED_BODY()
+
+	virtual void Interact_Implementation(APawn* InstigatorPawn) override;
+	
+public:	
+	// Sets default values for this actor's properties
+	ACTItemChest();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* LidMesh;
+
+	UPROPERTY(EditAnywhere)
+	int TargetPitch;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
