22a01f584e56fdfe9fb2792022e4bb287a3b88f2
Caio Teixeira <caioteixeira.dev@gmail.com>
2023-07-10 17:34:00 -0300

    Add Logic on Behavior Tree to have bot following player

---
+++ Source/ActionRogueLike/Private/AI/CTBTService_CheckAttackRange.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/CTBTService_CheckAttackRange.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void UCTBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	// Check distance between AI Pawn and target actor
+
+	auto* BlackboardComponent = OwnerComp.GetBlackboardComponent();
+	if (ensure(BlackboardComponent))
+	{
+		AActor* TargetActor = Cast<AActor>(BlackboardComponent->GetValueAsObject("TargetActor"));
+		if (TargetActor)
+		{
+			AAIController* MyController = OwnerComp.GetAIOwner();
+			if (ensure(MyController))
+			{
+				APawn* AIPawn = MyController->GetPawn();
+				if (ensure(AIPawn))
+				{
+					const float Distance = FVector::Distance(TargetActor->GetActorLocation(), AIPawn->GetActorLocation());
+					bool bWithinRange = Distance < 2000.f;
+
+					bool bHasLOS = false;
+					if (bWithinRange)
+					{
+						bHasLOS = MyController->LineOfSightTo(TargetActor);
+					}
+					
+					BlackboardComponent->SetValueAsBool(AttackRangeKey.SelectedKeyName, bWithinRange && bHasLOS);
+				}
+			}
+		}
+	}
+}
