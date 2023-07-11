f12bde80225e62c1fcde3692fc5e1206448b8a3f
Caio Teixeira <caioteixeira.dev@gmail.com>
2023-07-11 09:59:06 -0300

    Setup ranged attack and environmental queries

---
+++ Source/ActionRogueLike/Private/AI/CTBTTask_RangedAttack.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/CTBTTask_RangedAttack.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+
+EBTNodeResult::Type UCTBTTask_RangedAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	const auto* MyController = OwnerComp.GetAIOwner();
+
+	if (ensure(MyController))
+	{
+		const ACharacter* MyPawn = Cast<ACharacter>(MyController->GetPawn());
+
+		if (MyPawn == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		const FVector MuzzleLocation = MyPawn->GetMesh()->GetSocketLocation("Muzzle_01");
+
+		const auto* BlackboardComp = OwnerComp.GetBlackboardComponent();
+		const AActor* TargetActor = Cast<AActor>(BlackboardComp->GetValueAsObject("TargetActor"));
+		if (TargetActor == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		const FVector Direction = TargetActor->GetActorLocation() - MuzzleLocation;
+		const FRotator MuzzleRotation = Direction.Rotation();
+
+		FActorSpawnParameters Params;
+		Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		const AActor* NewProj = GetWorld()->SpawnActor<AActor>(ProjectileClass, MuzzleLocation, MuzzleRotation, Params);
+		return NewProj ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+	}
+
+	return EBTNodeResult::Failed;
+}
---
+++ Source/ActionRogueLike/Public/AI/CTBTTask_RangedAttack.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "CTBTTask_RangedAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API UCTBTTask_RangedAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category="AI")
+	TSubclassOf<AActor> ProjectileClass;
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
