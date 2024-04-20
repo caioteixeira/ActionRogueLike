e364cfd224a7c919b26cf682a5db0fd0f5726b3a
Caio Teixeira <caioteixeira.dev@gmail.com>
2024-04-20 16:50:55 -0300

    Fix damage direction calculation

--- Source/ActionRogueLike/Private/CTGamePlayFunctionLibrary.cpp
+++ Source/ActionRogueLike/Private/CTGamePlayFunctionLibrary.cpp
@@ -23,7 +23,10 @@ bool UCTGamePlayFunctionLibrary::ApplyDirectionalDamage(AActor* DamageCauser, AA
 		UPrimitiveComponent* HitComponent = HitResult.GetComponent();
 		if (HitComponent && HitComponent->IsSimulatingPhysics(HitResult.BoneName))
 		{
-			HitComponent->AddImpulseAtLocation(-HitResult.ImpactNormal * 300000.f, HitResult.ImpactPoint, HitResult.BoneName);
+			// Target - Origin
+			FVector Direction = HitResult.TraceEnd - HitResult.TraceStart;
+			Direction.Normalize();
+			HitComponent->AddImpulseAtLocation(Direction * 300000.f, HitResult.ImpactPoint, HitResult.BoneName);
 		}
 		return true;
 	}
